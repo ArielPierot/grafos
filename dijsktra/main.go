0d4a3baa81b55c017a8ba216e29e87e9d60b98b1
Ariel <arielpierot@gmail.com>
2020-01-23 20:41:29 -0300

    Alterado comentários

--- dijsktra/main.go
+++ dijsktra/main.go
@@ -15,11 +15,11 @@ func main() {
 
 	var array [][]int
 
-	arquivoEntrada := os.Args[1] // Entrada
+	arquivoEntrada := os.Args[1] // Entrada -- O primeiro argumento entra como parametro da localização do arquivo de entrada
 
 	array, V = entraArquivo(arquivoEntrada, array)
 
-	dijsktra(array, 0)
+	dijsktra(array, 0) // entrada do array e o vértice de origem
 }
 
 func distanciaMin(distArray []int, isLower []bool) int {
@@ -36,24 +36,24 @@ func distanciaMin(distArray []int, isLower []bool) int {
 	return min_index
 }
 
-func dijsktra(grafo [][]int, origem int){
+func dijsktra(grafo [][]int, origem int) {
 
 	dist := make([]int, V)
 	isLower := make([]bool, V)
 
-	for i:=1; i < V; i++ {
+	for i := 1; i < V; i++ {
 		dist[i] = math.MaxInt32
 		isLower[i] = false
 	}
 
 	dist[origem] = 0
 
-	for contador := 0; contador < V;  contador++ {
+	for contador := 0; contador < V; contador++ {
 		u := distanciaMin(dist, isLower)
 		isLower[u] = true
 
-		for p:=0; p<V; p++ {
-			if !isLower[p] && grafo[u][p] != 0 && dist[u] != math.MaxInt32 && dist[u] + grafo[u][p] < dist[p] {
+		for p := 0; p < V; p++ {
+			if !isLower[p] && grafo[u][p] != 0 && dist[u] != math.MaxInt32 && dist[u]+grafo[u][p] < dist[p] {
 				operacoes++
 				dist[p] = dist[u] + grafo[u][p]
 			}
@@ -71,12 +71,12 @@ func entraArquivo(arquivoEntrada string, array [][]int) ([][]int, int) {
 	for scanner.Scan() {
 		MAX++
 		input := scanner.Text()
-		arraySeparado := strings.Split(input,",")
+		arraySeparado := strings.Split(input, ",")
 
 		var linha []int
 
 		for _, v := range arraySeparado {
-			s, _ := strconv.Atoi(v);
+			s, _ := strconv.Atoi(v)
 			linha = append(linha, s)
 		}
 		array = append(array, linha)
@@ -86,10 +86,10 @@ func entraArquivo(arquivoEntrada string, array [][]int) ([][]int, int) {
 	return array, MAX
 }
 
-func exibirSolucao(dist []int)  {
+func exibirSolucao(dist []int) {
 	fmt.Println("Vértice \t Distância do vértice")
-	for i:=0; i < V; i++ {
-		fmt.Println(i, "\t\t",dist[i])
+	for i := 0; i < V; i++ {
+		fmt.Println(i, "\t\t", dist[i])
 	}
 
 	fmt.Println("Operações: " + strconv.Itoa(operacoes))
